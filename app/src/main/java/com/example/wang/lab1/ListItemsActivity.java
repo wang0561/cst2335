5c67cd205f0db2eb33187e6f771b44364a8f50af
Wang <zbwangtao1985@gmail.com>
2017-01-19 10:45:11 -0500

    Added three activity for lab2

--- app/src/main/java/com/example/wang/lab1/ListItemsActivity.java
+++ app/src/main/java/com/example/wang/lab1/ListItemsActivity.java
@@ -3,12 +3,11 @@ package com.example.wang.lab1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StartActivity extends AppCompatActivity {
+public class ListItemsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
-
+        setContentView(R.layout.activity_list_items);
     }
 }
